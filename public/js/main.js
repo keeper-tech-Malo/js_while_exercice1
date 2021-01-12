3008f7a88edd894a9830a57ecce8c966c09bd259
Malo <ndekezi37@gmail.com>
2021-01-12 09:20:40 +0100

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,42 @@
-// // exo1
-// let nrb = prompt('un nombre?')
-// let compt = 0;
-// while (compt <= nrb) {
-//     console.log(compt);
-//     compt++
+console.log("_____1_____");
+//1 
+// let nbMax = parseInt(prompt('Nombre?'));
+// let compt = 0; 
+
+// while (compt <= nbMax) {
+//         console.log(compt);
+//         compt++
 // }
 
-// exo2
+console.log("____2.1____");
+//2.1
 
+// let arret = 4;
+// let compt2 = 0;
 
+// while (compt2<=arret) {
+//     console.log(compt2);
+//     compt2++
+// }
+
+console.log("____2.2____");
+//2.2
+
+// let arret = 6;
+// let compt = 3;
+
+// while (compt <= arret) {
+//     console.log(compt);
+//     compt++      // compt = compt +1
+// }
+
+console.log("____2.3____");
+//2.3
+
+let arret = 8;
+let compt = 0;
+
+while (compt <= arret) {
+    console.log(compt);
+    compt += 2;     // compt = compt +2
+}
